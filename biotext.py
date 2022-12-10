e21954e155b8c1768da843046f2ffa34e4334f25
MrMiquy <mrmiquy@gmail.com>
2022-12-10 23:43:43 +0100

    TextStyle Class

--- biotext.py
+++ biotext.py
@@ -1,9 +1,3 @@
-# А Б В Г Ґ Д Е Є Ж З И І Ї Й К Л М 
-#   Н О П Р С Т У Ф Х Ц Ч Ш Щ Ь Ю Я
-
-# а б в г ґ д е є ж з и і ї й к л м 
-#   н о п р с т у ф х ц ч ш щ ь ю я
-
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap, QImage
 import PyQt5.QtCore as QtCore
@@ -18,25 +12,32 @@ import math
 import functools
 
 
-LARGE_UPCASES = ['Б', 'Г', 'Ґ', 'Д', 'Ж', 'И', 
-                 'Й', 'М', 'О', 'П', 'У', 'Ф', 
-                 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ю']
-
-class TextDraw():
-    def __init__(self, image: Image, fontsize = 550, thickness = 4, curl = [2, 5], cursive = 10, color = "red"):
+class TextStyle():
+    def __init__(self, fontsize = 0, cursive = 5, thickness = 2, curl = 5, color = "#131315"):
         self.fontsize = fontsize
-        self.curl = curl
         self.cursive = cursive
         self.thickness = thickness
+        self.curl = curl
         self.color = color
-        self.canvas = ImageDraw.Draw(image);
+
+        self.xPos = 0
+        self.yPos = 0
+
+class TextDraw():
+    def __init__(self, style: TextStyle):
+        self.im = None
+        self.canvas = None
         self.last_pos = []
         self.divide_random = [18, 22]
+        if style:
+            self.style = style
+        else:
+            self.style = TextStyle()
 
     def func(self, cords: list, t):
         new_cords = []
         for cord in cords:
-            new_cords.append(((cord[0] * self.fontsize) / 50, (cord[1] * self.fontsize) / 50))
+            new_cords.append(((cord[0] * self.style.fontsize) / 50, (cord[1] * self.style.fontsize) / 50))
 
         cords = new_cords
         return (
@@ -54,17 +55,20 @@ class TextDraw():
 
             while (t > 0):
                 bezier = self.func(cord, t)
-                curve.append((bezier[0], bezier[1] + math.pow(t + self.thickness, math.cos(t))))
+                curve.append((bezier[0], bezier[1] + math.pow(t, math.cos(t))))
                 t -= 0.01
 
             self.last_pos = [curve[-1][0], curve[-1][1]]
-            self.canvas.polygon(curve, self.color)
+            self.canvas.polygon(curve, self.style.color)
 
-    def divide_func(self, xy: tuple, xy2: tuple, use_random = True):
+    def divide_func(self, xy: tuple, xy2: tuple, use_random = True, xy3_4 = None):
         if use_random == 66:
-            return ((xy[0] + xy2[0]) / (randint(self.divide_random[0], self.divide_random[1]) / 10), (xy2[0] + xy2[1]) / (randint(self.divide_random[0], self.divide_random[1]) / 10))
+            return ((xy[0] - xy2[0]) / (randint(self.divide_random[0], self.divide_random[1]) / 10), (xy2[1] - xy2[1]) / (randint(self.divide_random[0], self.divide_random[1]) / 10))
         else:
-            return ((xy[0] + xy2[0]), (xy2[0] + xy2[1]))
+            if xy3_4 == None:
+                return ((xy[0] + xy2[0]) / 2, (xy2[1] + xy2[1]) / 2)
+            else:
+                return ((xy[0] + xy2[0]) / 2 - ((xy3_4[0] + xy3_4[0]) / 2) / 2, (xy2[1] + xy2[1]) / 2 - ((xy3_4[1][1] + xy3_4[1][1]) / 2) / 2)
 
     def divide(self, cords: list, divides):
         new_cords = []
@@ -74,19 +78,19 @@ class TextDraw():
                 cords_list.append((cord[0][0], cord[0][1]))
                 cords_list.append(self.divide_func(cord[0], cord[1]))
                 cords_list.append((cord[1][0], cord[1][1]))
-                cords_list.append(self.divide_func(cord[1], cord[2], False))
-                cords_list[1] = (cords_list[1][0] + self.cursive, cords_list[1][1])
-                cords_list[2] = (cords_list[2][0] + self.cursive, cords_list[2][1])
+                cords_list.append(self.divide_func(cord[1], cord[2]))
+                cords_list[2] = (cords_list[2][0] + self.style.cursive, cords_list[2][1])
+                cords_list[3] = (cords_list[3][0] + self.style.cursive, cords_list[3][1])
 
                 new_cords.append(cords_list)
 
                 cords_list = []
-                cords_list.append(self.divide_func(cord[1], cord[2], False))
+                cords_list.append(self.divide_func(cord[1], cord[2]))
                 cords_list.append((cord[2][0], cord[2][1]))
                 cords_list.append(self.divide_func(cord[2], cord[3]))
                 cords_list.append((cord[3][0], cord[3][1]))
-                cords_list[1] = (cords_list[1][0] + self.cursive, cords_list[1][1])
-                cords_list[2] = (cords_list[2][0] + self.cursive, cords_list[2][1])
+                cords_list[0] = (cords_list[0][0] + self.style.cursive, cords_list[0][1])
+                cords_list[1] = (cords_list[1][0] + self.style.cursive, cords_list[1][1])
                 
                 new_cords.append(cords_list)
                 
@@ -102,21 +106,34 @@ class TextDraw():
         self.curve(cords, divide)
 
     def draw(self, letter: str):
+        w = len(max(letter.split('\n'))) * self.style.fontsize
+        h = len(letter.split('\n')) * self.style.fontsize
+
+        self.im = Image.new("RGBA", (w*2, h*2), color=(0,0,0,0))
+        self.canvas = ImageDraw.Draw(self.im, "RGBA")
+
         if letter == 'A' or letter == 'А':
-            self.curve([(25, 100), (20, 0), (80, 0), (75, 100)])
+            self.curve([(35, 90), (24, 15), (70, 10), (65, 90)], 1)
             #self.curve([(0, 0), (2,5), (2,6), (2,6)])
-    
-    def clear(self, image: Image):
-        self.canvas.rectangle((0, 0, image.width, image.height), fill=(0, 0, 0, 0))
+        self.im = self.im.resize((w, h), Image.ANTIALIAS)
+        return self.im
 
 class BioText(Ui_MainWindow, QMainWindow):
-    def __init__(self):
+    def __init__(self, text_style: TextStyle = None):
         super(Ui_MainWindow, self).__init__()
         self.ui = self.setupUi(self)
         self.source_image = None        # QPixmap          // user image
         self.source_pil = None
-        self.overlay_image = None       # Pillow Image     // where text will be rendered
-        self.obj = None                 # TextDraw Class   // text render class
+        self.draw = TextDraw(text_style)                 # TextDraw Class   // text render class
+        
+        if text_style:
+            self.sSize.setValue(text_style.fontsize)
+            self.sCursive.setValue(text_style.cursive)
+            self.sThickness.setValue(text_style.thickness)
+            self.sCurl.setValue(text_style.curl)
+
+            self.sXPos.setValue(text_style.xPos)
+            self.sYPos.setValue(text_style.yPos)
 
         # connections / signals
         self.textEdit.textChanged.connect(self.text_changed)
@@ -129,30 +146,46 @@ class BioText(Ui_MainWindow, QMainWindow):
         self.actionText.triggered.connect(self.save_text)
 
         # spinboxes -- Style
+        self.sSize.valueChanged.connect(self.fontsize_changed)
+        self.sCursive.valueChanged.connect(self.spinbox_changed)
+        self.sThickness.valueChanged.connect(self.thickness_changed)
+        self.sCurl.valueChanged.connect(self.curl_changed)
+
+        self.sSizeMin.valueChanged.connect(self.spinbox_changed)
+        self.sSizeMax.valueChanged.connect(self.spinbox_changed)
+        self.sCursiveMin.valueChanged.connect(self.spinbox_changed)
+        self.sCursiveMax.valueChanged.connect(self.spinbox_changed)
+        self.sThicknessMin.valueChanged.connect(self.spinbox_changed)
+        self.sThicknessMax.valueChanged.connect(self.spinbox_changed)
+        self.sCurlMin.valueChanged.connect(self.spinbox_changed)
+        self.sCurlMax.valueChanged.connect(self.spinbox_changed)
+        self.sDivide.valueChanged.connect(self.spinbox_changed)
         
         # spinboxes -- Text
-        self.sXPos.valueChanged.connect(self.x_pos_value_changed)
-        self.sYPos.valueChanged.connect(self.y_pos_value_changed)
+        self.sXPos.valueChanged.connect(self.xPos_changed)
+        self.sYPos.valueChanged.connect(self.yPos_changed)
+        self.sWidth.valueChanged.connect(self.spinbox_changed)
+        self.sHeight.valueChanged.connect(self.spinbox_changed)
+        self.ax1.valueChanged.connect(self.spinbox_changed)
 
     def redraw_overlay(self):
         if self.source_image and not self.source_image.isNull():
-            self.obj.clear(self.overlay_image)
             if self.textEdit.toPlainText():
-                self.obj.draw(self.textEdit.toPlainText().split("\n")[0][0])
+                overlay = self.draw.draw(self.textEdit.toPlainText().split("\n")[0][0])
+                source = self.source_pil.copy()
+                source.paste(overlay, (self.sXPos.value(), self.sYPos.value()), overlay)
+                source = ImageQt(source)
 
-            source = self.source_pil
+                pixmap = QPixmap.fromImage(source)
+                pixmap = pixmap.scaled(self.lImage.width(), self.lImage.height(), QtCore.Qt.KeepAspectRatio)
+                self.lImage.setPixmap(pixmap)
 
-            overlay = self.overlay_image.resize((self.overlay_image.width // 2, self.overlay_image.height // 2), resample=Image.ANTIALIAS)
-            source.paste(overlay, (self.sXPos.value(), self.sYPos.value()), overlay)
-            source = ImageQt(source)
-
-            pixmap = QPixmap.fromImage(source)
-            pixmap = pixmap.scaled(self.lImage.width(), self.lImage.height(), QtCore.Qt.KeepAspectRatio)
-            self.lImage.setPixmap(pixmap)
-
-    def open_image(self, event):
+    def open_image(self, event = None, path = None):
         if not self.source_image or self.source_image.isNull():
-            imagePath, _ = QFileDialog.getOpenFileName()
+            if not path:
+                imagePath, _ = QFileDialog.getOpenFileName()
+            else:
+                imagePath = path
             if imagePath:
                 self.source_image = QPixmap(imagePath)
                 if not self.source_image.isNull():
@@ -161,32 +194,28 @@ class BioText(Ui_MainWindow, QMainWindow):
                     buffer.open(QBuffer.ReadWrite)
                     self.source_pil.save(buffer, 'PNG')
                     self.source_pil = Image.open(io.BytesIO(buffer.data()))
-                    self.overlay_image = Image.new('RGBA', (self.source_image.width()*2, self.source_image.height()*2), (0, 0, 0, 0)) 
-                    self.obj = TextDraw(self.overlay_image)
-                    self.resize_source_image(0)
+                    if self.draw.style.fontsize == 0:
+                        self.draw.style.fontsize = self.source_pil.height // 20
+                        self.sSize.setValue(self.draw.style.fontsize)
+                    self.resize_source_image()
     
-    def resize_source_image(self, event):
+    def resize_source_image(self, event = None):
         if self.source_image:
             self.redraw_overlay()
 
     def export(self):
-        if self.source_image and self.overlay_image:
+        if self.source_image and self.textEdit.toPlainText():
             imagePath, _ = QFileDialog.getSaveFileName(self,"Export to...","","png;;jpg;;gif;;bpm;;jpeg;;webp")
+
             if imagePath:
-                self.obj.clear(self.overlay_image)
-                if self.textEdit.toPlainText():
-                    self.obj.draw(self.textEdit.toPlainText().split("\n")[0][0])
+                overlay = self.draw.draw(self.textEdit.toPlainText().split("\n")[0][0])
+                source = self.source_pil
+                source.paste(overlay, (self.sXPos.value(), self.sYPos.value()), overlay)
 
-                    source = self.source_pil
-                    overlay = self.overlay_image
-                    overlay = overlay.resize((self.overlay_image.width // 2, self.overlay_image.height // 2), resample=Image.ANTIALIAS)
-                    overlay = overlay.filter(ImageFilter.SMOOTH)
-
-                    source.paste(overlay, (self.sXPos.value(), self.sYPos.value()), overlay)
-                    _ = _.strip()
-                    if _ in ['jpg', 'jpeg', 'bmp']:
-                        source = source.convert('RGB')
-                    source.save(imagePath + "." + _)
+                _ = _.strip()
+                if _ in ['jpg', 'jpeg', 'bmp']:
+                    source = source.convert('RGB')
+                source.save(imagePath + "." + _)
 
     def save_text(self):
         if self.textEdit.toPlainText():
@@ -203,75 +232,39 @@ class BioText(Ui_MainWindow, QMainWindow):
     def value_changed(method):
         @functools.wraps(method)
         def wrapper(self):
-            method(self)
-            self.redraw_overlay()
+            try: 
+                method(self)
+                self.redraw_overlay()
+            except:
+                pass
         return wrapper
 
-    # spinboxes
     @value_changed
-    def fontsize_value_changed(self):
+    def spinbox_changed(self):
         pass
 
     @value_changed
-    def cursive_value_changed(self):
-        pass
+    def fontsize_changed(self):
+        self.draw.style.fontsize = self.sSize.value()
 
     @value_changed
-    def thickness_value_changed(self):
-        pass
+    def cursive_changed(self):
+        self.draw.style.cursive = self.sCursive.value()
 
     @value_changed
-    def thickness_value_changed(self):
-        pass
+    def thickness_changed(self):
+        self.draw.style.thickness = self.sThickness.value()
 
     @value_changed
-    def x_pos_value_changed(self):
-        pass
+    def curl_changed(self):
+        self.draw.style.curl = self.sCurl.value()
 
     @value_changed
-    def y_pos_value_changed(self):
-        pass
+    def xPos_changed(self):
+        self.draw.style.xPos = self.sXPos.value()
 
     @value_changed
-    def thickness_value_changed(self):
-        pass
+    def yPos_changed(self):
+        self.draw.style.yPos = self.sYPos.value()
 
-    @value_changed
-    def resolution_width_value_changed(self):
-        pass
-
-    @value_changed
-    def resolution_height_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_x1_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_y1_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_x2_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_y2_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_x3_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_y3_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_x4_value_changed(self):
-        pass
-
-    @value_changed
-    def anchor_y4_value_changed(self):
-        pass
+        #self.obj.style.color = self.sSize.value()
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from biotext import BioText
+from biotext import BioText, TextStyle
 
 from PyQt5.QtWidgets import QApplication
 from sys import argv
 
+style = TextStyle()
+style.fontsize = 200
+style.xPos = 32
+
 app = QApplication(argv)
-win = BioText()
+win = BioText(style)
+
+win.textEdit.setText('A')
+win.open_image(path = 'test-image.png')
+#win.export(just_show = True)
 win.show()
 app.exec_()
